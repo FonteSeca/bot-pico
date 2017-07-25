ea0ad81ec960b115903c3cc88c7f7de839fa1511
yFonteSeca <lf.caetanofonseca@outlook.com>
2017-07-25 16:07:30 -0300

    asd

--- index.js
+++ index.js
@@ -189,7 +189,7 @@ oie.kick();
   }
 
   if ((/desu/).test(message.content)) {
-    console.log(message);
+    console.log(message.content);
     message.delete(20)
     .then(msg => console.log(`Deleted message from ${msg.author}`))
     .catch(console.error);
