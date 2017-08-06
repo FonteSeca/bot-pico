4d6a06dc94bf4943ad17e745d074e1a84ad194f6
yFonteSeca <lf.caetanofonseca@outlook.com>
2017-08-06 10:33:21 -0300

    asd

--- index.js
+++ index.js
@@ -208,7 +208,7 @@ oie.kick();
   }
 
   if ((/kyaaa/).test(message.content)) {
-    console.log(message.author + ': ' + message.content);
+    console.log(message.author.toString() + ': ' + message.content);
     console.log(message.username);
     message.delete(500)
     .then(msg => console.log(`Deleted message from ${msg.author}`))
