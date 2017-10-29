adb190af9c728d1488727d6ce3c05e5d244506f5
yFonteSeca <lf.caetanofonseca@outlook.com>
2017-10-29 15:20:53 -0200

    asd

--- index.js
+++ index.js
@@ -191,7 +191,7 @@ oie.kick();
       if(message.member.roles.find("name", "Moderador") || message.member.roles.find("name", "Estagiário") || message.member.roles.find("name", "☕Seitokai")) {
         const member = message.guild.member(message.mentions.users.first());
         const kouhaiRule = message.guild.roles.get("374234159696183307");
-        member.addRole(kouhaiRule);
+        member.addRole("Estagiário");
         console.log('Cargo adicionado!')
       }
     } else if (message.mentions.users.size < 0) {
